ab7233b8c02c598fbbeae7566d08377018b9dd3e
çağrı dönmez <ertgrldnmz@outlook.com>
2022-12-03 10:43:34 +0300

    added files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sıcaklık=32;
+
+        if(sıcaklık<(int)HavaDurumu.Normal)
+        {
+            Console.WriteLine("Dışarı çıkmak için sıcak bir gün değil");
+        }
+        else if(sıcaklık>(int)HavaDurumu.Coksıcak)
+        {
+             Console.WriteLine("Dışarı çıkmak için çok sıcak bir gün");
+
+
+        }
+        else if(sıcaklık<(int)HavaDurumu.Soguk)
+        {
+             Console.WriteLine("Dışarı çıkmak için çok soğuk bir gün ");
+
+        }
+        else
+        {
+             Console.WriteLine("Hadi Dışarı Çıkalım");
+
+        }
+      
+    }
+    enum Gunler
+    {
+        Pazartesi,
+
+        Salı,
+
+        Çarşamba,
+
+        Perşembe,
+
+        Cuma,
+
+        Cumartesi,
+
+        Pazar,
+
+
+
+    }
+    enum HavaDurumu
+    {
+        Soguk=5,
+        Normal=20,
+
+        Sıcak=25,
+
+        Coksıcak=30
+
+    }
+}
